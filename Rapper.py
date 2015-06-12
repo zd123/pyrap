69e77fea1fdb1b5ab551e95974a214a1ceb83835
King Zack <zacharydesario@gmail.com>
2015-06-11 23:15:08 -0700

    updating with more type of hot spitters, ie the bi and unigram mcmc spitters

--- Rapper.py
+++ Rapper.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+from collections import defaultdict, deque
 from textblob import TextBlob
 import pandas as pd
 import numpy as np
@@ -11,11 +11,13 @@ except:
    import pickle
 
 class Rapper(object):
-    def __init__(self, style='styles/default_default.pkl'):
-        self.vocab_dict = pickle.load(open( "styles/defualt_style.pkl", "rb" ) )
+    def __init__(self, pos_style='styles/default_pos_dict.pkl', mcmc_style='styles/default_mcmc_dict.pkl'):
+        self.pos_dict = pickle.load(open( pos_style, "rb" ) )
+        self.mcmc_dict = pickle.load(open( mcmc_style, "rb" ) )
+        self.mcmc_dict_unigram = pickle.load(open( 'styles/default_mcmc_dict_unigram.pkl', "rb" ) )
         print 'Chyeahhh boiiiii!, welcome to pyrapper'
 
-    def format_for_spew(self, the_string=''):
+    def format_for_pos_spit(self, the_string=''):
         the_string = unicode(the_string, 'utf-8')
         blob = TextBlob(the_string)
 
@@ -26,14 +28,13 @@ class Rapper(object):
 
         return lst_of_pos_tags
 
-    def spit(self, sentnece='We rollin, the hatin'):
 
-        lst_of_pos_tags = self.format_for_spew(sentnece)
-
+    def pos_spit(self, sentnece='We rollin, they hatin'):
+        lst_of_pos_tags = self.format_for_pos_spit(sentnece)
         final_sentence = []
 
         for tag in lst_of_pos_tags:
-            limit = len(self.vocab_dict[tag])
+            limit = len(self.pos_dict[tag])
             # If for some reason there are no words associated with a tag, skip it.
             if not limit:
                 continue
@@ -42,7 +43,7 @@ class Rapper(object):
             random_choice = np.random.randint(0, limit)
 
             # Pluck out the random word.
-            chosen_word = self.vocab_dict[tag][random_choice]
+            chosen_word = self.pos_dict[tag][random_choice]
 
             # Appned it to the final sentnece.
             final_sentence.append(chosen_word)
@@ -53,5 +54,122 @@ class Rapper(object):
         return final_sentence
 
 
-rapper = Rapper()
-print rapper.spit('hey ladies ladies you wanna ride in my Mercades??')
+
+    def format_for_mcmc_unigram_spit(self, the_string=''):
+        the_string = unicode(the_string, 'utf-8')
+        words_shifted_zero = the_string.split(' ')
+        words_shifted_one = the_string.split(' ')
+
+        words_shifted_zero = deque(words_shifted_zero)
+        words_shifted_one = deque(words_shifted_one)
+
+        # words_shifted_zero.rotate(0)
+        words_shifted_one.rotate(-1)
+        lst_of_unigram_tuples = []
+
+        # print words_shifted_one
+        # print "Words_shifte_one above"
+        # print '='*80
+
+        for zero, one in zip(words_shifted_zero, words_shifted_one):
+            lst_of_unigram_tuples.append((zero))
+        return lst_of_unigram_tuples
+
+
+
+    def format_for_mcmc_spit(self, the_string=''):
+        the_string = unicode(the_string, 'utf-8')
+        words_shifted_zero = the_string.split(' ')
+        words_shifted_one = the_string.split(' ')
+
+        words_shifted_zero = deque(words_shifted_zero)
+        words_shifted_one = deque(words_shifted_one)
+
+        # words_shifted_zero.rotate(0)
+        words_shifted_one.rotate(-1)
+        lst_of_bigram_tuples = []
+
+        # print words_shifted_one
+        # print "Words_shifte_one above"
+        # print '='*80
+
+        for zero, one in zip(words_shifted_zero, words_shifted_one):
+            lst_of_bigram_tuples.append( (zero,one) )
+        return lst_of_bigram_tuples
+
+
+    def mcmc_unigram_spit(self, sentence='We rollin, they hatin'):
+        lst_of_unigram_tuples = self.format_for_mcmc_unigram_spit(sentence)
+        final_sentence = []
+        # print lst_of_bigram_tuples
+        # print "lst of bigrap tupes above"
+        # print '='*80
+        for tag in lst_of_unigram_tuples:
+            limit = len(self.mcmc_dict_unigram[tag])
+
+            # If for some reason there are no words associated with a tag, skip it.
+            if not limit:
+                continue
+
+            # Chose a random interger index that is within the bounds of the
+            # lenght of the limit
+            random_choice = np.random.randint(0, limit)
+
+            # Pluck out the random word.
+            chosen_word = self.mcmc_dict_unigram[tag][random_choice]
+
+            if not chosen_word:
+                continue
+
+            # Appned it to the final sentnece.
+            final_sentence.append(chosen_word)
+
+        final_sentence = ' '.join(final_sentence)
+
+        return final_sentence
+
+
+    def mcmc_spit(self, sentence='We rollin, they hatin'):
+        lst_of_bigram_tuples = self.format_for_mcmc_spit(sentence)
+        final_sentence = []
+        # print lst_of_bigram_tuples
+        # print "lst of bigrap tupes above"
+        # print '='*80
+        for tag in lst_of_bigram_tuples:
+            limit = len(self.mcmc_dict[tag])
+
+            # If for some reason there are no words associated with a tag, skip it.
+            if not limit:
+                continue
+
+            # Chose a random interger index that is within the bounds of the
+            # lenght of the limit
+            random_choice = np.random.randint(0, limit)
+
+            # Pluck out the random word.
+            chosen_word = self.mcmc_dict[tag][random_choice]
+
+            if not chosen_word:
+                continue
+
+            # Appned it to the final sentnece.
+            final_sentence.append(chosen_word)
+
+        final_sentence = ' '.join(final_sentence)
+
+        return final_sentence
+
+
+
+rp = Rapper()
+original = "Ohh shit we started up here and now we really doin it my nigga be so hollarin so hard yo"
+print original
+print '-'*80
+print "\noriginal above, computer mcmc rap below"
+print '-'*80
+print rp.mcmc_spit(original)
+print '-'*80
+print "\n pos_rap below"
+print rp.pos_spit(original)
+print "\n unigram mcmc_below"
+print rp.mcmc_unigram_spit(original)
--- StyleCreator.py
+++ StyleCreator.py
@@ -1,34 +1,34 @@
-from collections import defaultdict
+from collections import defaultdict, deque
 from textblob import TextBlob
-import pandas as pd
 import numpy as np
 import re
-import nltk
+
 try:
    import cPickle as pickle
 except:
    import pickle
 
 class StyleCreator(object):
-    def __init__(self, path_to_text='styles/drizzy.txt'):
+    def __init__(self, path_to_text='styles/default.txt'):
         self.path_to_text = path_to_text
         self.text = ''
         with open(path_to_text) as fh:
             self.text = fh.read()
 
-        self.create_vocab()
+        self.pos_dict = self.create_pos_dict()
+        self.mcmc_dict = self.create_mcmc_dict()
 
-    def create_vocab(self):
+    def create_pos_dict(self):
         '''
         INPUT: String of all text that you want to build the lexicon with.
-        OUTPUT: A pickled object of the vocab_dict, that is the small heart of pyrapper and vocab_dict
-        print vocab_dict [out] { 'JJ': ['big', 'huge', 'big', 'small', 'soft', 'rocky', 'big'], 'NN': ['dog', 'lawn', 'dog', 'dogs'] ... }
+        OUTPUT: A pickled object of the pos_dict, that is the small heart of pyrapper
+        print pos_dict [out] { 'JJ': ['big', 'huge', 'big', 'small', 'soft', 'rocky', 'big'], 'NN': ['dog', 'lawn', 'dog', 'dogs'] ... }
         '''
 
         # Convert text to unicode to aviod errors during the tagging process
         self.text = unicode(self.text, 'utf-8')
 
-        vocab_dict = defaultdict(list)
+        pos_dict = defaultdict(list)
 
         # blobify our string
         blob = TextBlob(self.text)
@@ -38,11 +38,60 @@ class StyleCreator(object):
 
         #  {'NN': [dog, cat, dog, human]}
         for word, tag in tagged:
-            if len(word) > 1:
-                vocab_dict[tag].append(word)
+            if len(word) >= 1: # <--- if not an empty string
+                pos_dict[tag].append(word)
 
-        filename_to_save_to = self.path_to_text[:-4] + '.pkl'
-        pickle.dump( vocab_dict, open( filename_to_save_to, "wb" ) )
-        return vocab_dict
+        filename_to_save_to = self.path_to_text[:-4] + '_pos_dict.pkl'
+        pickle.dump( pos_dict, open( filename_to_save_to, "wb" ) )
+        print "saved %s " % filename_to_save_to
 
-StyleCreator()
+        return pos_dict
+
+    def create_mcmc_dict_unigram(self):
+        self.text = self.text.encode('ascii', 'ignore')
+        mcmc_dict = defaultdict(list)
+
+        words_shifted_zero = self.text.split(' ')
+        words_shifted_one = self.text.split(' ')
+
+
+        words_shifted_zero = deque(words_shifted_zero)
+        words_shifted_one = deque(words_shifted_one)
+
+        # words_shifted_zero.rotate(0)
+        words_shifted_one.rotate(-1)
+
+
+        for zero, one in zip(words_shifted_zero, words_shifted_one):
+            mcmc_dict[zero].append(one)
+
+        filename_to_save_to = self.path_to_text[:-4] + '_mcmc_dict_unigram.pkl'
+        pickle.dump( mcmc_dict, open( filename_to_save_to, "wb" ) )
+
+    def create_mcmc_dict(self):
+        self.text = self.text.encode('ascii', 'ignore')
+        mcmc_dict = defaultdict(list)
+
+        words_shifted_zero = self.text.split(' ')
+        words_shifted_one = self.text.split(' ')
+        words_shifted_two = self.text.split(' ')
+
+        words_shifted_zero = deque(words_shifted_zero)
+        words_shifted_one = deque(words_shifted_one)
+        words_shifted_two = deque(words_shifted_two)
+        # words_shifted_zero.rotate(0)
+        words_shifted_one.rotate(-1)
+        words_shifted_two.rotate(-2)
+
+        for zero, one, two in zip(words_shifted_zero, words_shifted_one, words_shifted_two):
+            mcmc_dict[(zero, one)].append(two)
+
+        filename_to_save_to = self.path_to_text[:-4] + '_mcmc_dict.pkl'
+        pickle.dump( mcmc_dict, open( filename_to_save_to, "wb" ) )
+
+        print "saved %s " % filename_to_save_to
+        return mcmc_dict
+
+#
+sc = StyleCreator()
+sc.create_mcmc_dict_unigram()
